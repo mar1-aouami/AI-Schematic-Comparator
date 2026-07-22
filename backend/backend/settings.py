70eba46cf632c1ff41865ff730cd1fc66f8ef5a2
mar1-eng <aouamimarouan@gmail.com>
2026-07-22 08:49:06 +0100

    Fix Celery SSL config pour rediss

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,6 +133,10 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
+if CELERY_BROKER_URL.startswith('rediss://'):
+    CELERY_BROKER_USE_SSL = {'ssl_cert_reqs': 'CERT_NONE'}
+    CELERY_REDIS_BACKEND_USE_SSL = {'ssl_cert_reqs': 'CERT_NONE'}
+
 
 # Gestion éphémère des fichiers médias (Stockage temporaire des plans)
 MEDIA_URL = '/media/'
